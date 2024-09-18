06c420314eef668a7941671c8f33a343e6298d56
MAHossain1 <mahpro110@gmail.com>
2024-09-18 07:53:11 +0600

    Added footer section.

--- data/index.ts
+++ data/index.ts
@@ -218,13 +218,16 @@ export const socialMedia = [
   {
     id: 1,
     img: '/git.svg',
+    link: 'https://github.com/MAHossain1',
   },
   {
     id: 2,
-    img: '/twit.svg',
+    img: '/x1.png',
+    link: 'https://x.com/mahpro110',
   },
   {
     id: 3,
     img: '/link.svg',
+    link: 'https://www.linkedin.com/in/arman-web-developer/',
   },
 ];
