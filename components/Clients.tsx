615c92b1663bc41f77cdb9909c8f3cb141e2db93
MAHossain1 <mahpro110@gmail.com>
2024-09-18 07:06:40 +0600

    Added my approach section.

--- components/Clients.tsx
+++ components/Clients.tsx
@@ -8,7 +8,7 @@ const Clients = () => {
       <h1 className="heading mt-4">
         Kind Words from <span className="text-purple">Clients</span>
       </h1>
-      <div className=" max-lg:mt-10 flex flex-col items-center">
+      <div className="mt-10  flex flex-col items-center">
         <InfiniteMovingCards
           items={testimonials}
           speed="slow"
